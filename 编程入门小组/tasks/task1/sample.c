9d005bc70eb6160424c95c001e3389ff4a6b59d9
superfree <181250003@smail.nju.edu.cn>
2019-10-23 13:20:23 +0800

    complete smaple in task 1

--- 编程入门小组/tasks/task1/sample.c
+++ 编程入门小组/tasks/task1/sample.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+int n[] = {0,1,2,3,4,5,6,7,8,9,10,15,20,30,50,100};
+int ans[] = {1,-1,1,1,1,2,2,3,4,5,7,28,114,1897,525456,961938845};
+int pass = TRUE;
+
 int climb_stairs(int n);
+void run_test();
+void print_result();
 
 int main() {
-  int n[] = {0,1,2,3,4,5,6,7,8,9,10,15,20,30,50,100};
-  int ans[] = {1,-1,1,1,1,2,2,3,4,5,7,28,114,1897,525456,961938845};
-  int pass = 1;
-  for(int i=0 ; i<16 ; i++) {
-    int result = climb_stairs(n[i]);
-    if(result == ans[i])
-      printf("n = %d, ans = %d\n", n[i], result);
-    else {
-      printf("n = %d, ans = %d, but expected = %d\n", n[i], result, ans[i]);
-      pass = 0;
-    }
-  }
-  if(pass)
-    printf("\npass\n");
-  else
-    printf("\nfail\n");
+
+  run_test();
+
 }
 
 int climb_stairs(int n) {
   // TODO
 
   return 0;
+}
+
+void run_test() {
+  int result;
+  for(int i=0 ; i<16 ; i++) {
+    result = climb_stairs(n[i]);
+    if(result == ans[i])
+      printf("n = %d, ans = %d\n", n[i], result);
+    else {
+      printf("n = %d, ans = %d, but expected = %d\n", n[i], result, ans[i]);
+      pass = FALSE;
+    }
+  }
+  print_result();
+}
+
+void print_result() {
+  if(pass)
+    printf("\npass\n");
+  else
+    printf("\nfail\n");
 }
