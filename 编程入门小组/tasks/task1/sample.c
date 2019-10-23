3cf9245ed43ae0ee1f9233d16cd2b956847c341e
superfree <181250003@smail.nju.edu.cn>
2019-10-23 13:23:03 +0800

    complete smaple in task 1

--- 编程入门小组/tasks/task1/sample.c
+++ 编程入门小组/tasks/task1/sample.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 
+// 全局常量声明
 #define TRUE 1
 #define FALSE 0
+
+// 测试数据定义
 int n[] = {0,1,2,3,4,5,6,7,8,9,10,15,20,30,50,100};
 int ans[] = {1,-1,1,1,1,2,2,3,4,5,7,28,114,1897,525456,961938845};
 int pass = TRUE;
 
+// 函数声明
 int climb_stairs(int n);
 void run_test();
 void print_result();
 
+// 主程序入口
 int main() {
 
   run_test();
 
 }
 
+// TODO
 int climb_stairs(int n) {
-  // TODO
+  // write your code here
 
   return 0;
 }
 
+// 测试函数
 void run_test() {
   int result;
   for(int i=0 ; i<16 ; i++) {
@@ -36,6 +43,7 @@ void run_test() {
   print_result();
 }
 
+// 打印测试结果函数
 void print_result() {
   if(pass)
     printf("\npass\n");
